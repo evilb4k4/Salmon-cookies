4a54e32be93f8e1c5b91c4c688bc6c89a9213c43
Adan.Rabadan <evilb4k4@gmail.com>
2017-04-11 16:45:20 -0700

    got the table to work but still working on the styling

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var storeHourArray = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm','Total'];
+var storeHourArray = ['','6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm','Total'];
 var app = document.getElementById('app');
 console.log(app);
 
@@ -69,17 +69,48 @@ CookieStore.prototype.fillTotal = function(){
 };
 CookieStore.prototype.listItem = function(){
   this.fillTotal();
-  console.log('body works', app);
-  var newHeading = document.createElement('h2');
-  app.appendChild(newHeading);
-  newHeading.innerText = this.name;
-  var hourlySales = document.createElement('ul');
-  for (var i = 0; i < storeHourArray.length; i++){
-    var newEL = document.createElement('li');
-    newEL.innerText = storeHourArray[i] + ': ' + this.cookieTotal[i] + ' cookies';
-    hourlySales.appendChild(newEL);
+  var table = document.getElementsByTagName('table')[0];
+  var tableRow = document.createElement('tr');
+  var tableBody = document.getElementsByTagName('tbody')[0];
+  var newTH = document.createElement('th');
+  newTH.innerText = this.name;
+  tableRow.appendChild(newTH);
+  for (var i = 0; i < this.cookieTotal.length; i++){
+    var hourlyTD = document.createElement('td');
+    hourlyTD.innerText = this.cookieTotal[i];
+    tableRow.appendChild(hourlyTD);
   };
-  app.appendChild(hourlySales);
+  tableBody.appendChild(tableRow);
+  table.appendChild(tableBody);
 };
+
+function createTable(){
+  var table = document.createElement('table');
+  app.appendChild(table);
+  var tablehead = document.createElement('thead');
+  table.appendChild(tablehead);
+  var tableRow = document.createElement('tr');
+  tablehead.appendChild(tableRow);
+  var tableBody = document.createElement('tbody');
+  table.appendChild(tableBody);
+  for (var i = 0; i < storeHourArray.length; i++){
+    var th = document.createElement('th');
+    tableRow.appendChild(th);
+    th.innerText = storeHourArray[i];
+  }
+}
+createTable();
+
+
+
+
 var pikeStore = new CookieStore('1st and Pike', 65, 23, 6.3);
 pikeStore.listItem();
+var seaTacStore = new CookieStore('SeaTac',24,3,1.2);
+seaTacStore.listItem();
+var seattleCenterStore = new CookieStore('Seattle Center',38,11,3.7);
+seattleCenterStore.listItem();
+var capitolHillStore = new CookieStore('Capitol Hill',38,20,2.3);
+capitolHillStore.listItem();
+var alkiStore = new CookieStore('Alki',16,2,4.6);
+alkiStore.listItem();
