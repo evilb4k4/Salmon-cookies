828a7584ed385ab034410e9995a7495ce1077a02
Adan.Rabadan <evilb4k4@gmail.com>
2017-04-12 16:46:49 -0700

    got my table working but still need styling

--- app.js
+++ app.js
@@ -2,54 +2,13 @@
 
 var storeHourArray = ['','6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm','Total'];
 var app = document.getElementById('app');
-console.log(app);
 
-// var firstAndPike = {
-//   mincustomer: 23,
-//   maxcustomer: 65,
-//   avgCookieHour: 6.3,
-//   cookieTotal: [],
-//   name: '1st and Pike',
-//   getRandomCustomer: function() {
-//     return Math.floor(Math.random() * (this.maxcustomer - this.mincustomer + 1)) + this.mincustomer;
-//   },
-//   cookiePerHour: function(){
-//     return Math.ceil(this.getRandomCustomer() * this.avgCookieHour);
-//   },
-//   fillTotal: function(){
-//     var total = 0;
-//     this.cookieTotal.length = 15;
-//     for (var i = 0; i < 15; i++){
-//       var thisHour = this.cookiePerHour();
-//       total += thisHour;
-//       this.cookieTotal[i] = thisHour;
-//     }
-//     this.cookieTotal.push(total);
-//   },
-//   listItem: function(){
-//     this.fillTotal();
-//     var newHeading = document.createElement('h2');
-//     body.appendChild(newHeading);
-//     newHeading.innerText = this.name;
-//     var hourlySales = document.createElement('ul');
-//     for (var i = 0; i < storeHourArray.length; i++){
-//       var newEL = document.createElement('li');
-//       newEL.innerText = storeHourArray[i] + ': ' + this.cookieTotal[i] + ' cookies';
-//       hourlySales.appendChild(newEL);
-//     };
-//     body.appendChild(hourlySales);
-//   }
-// };
-// firstAndPike.listItem();
-
-// starting my constructor function
 function CookieStore(name, maxCustomer, minCustomer, avgCookiePerHour) {
   this.name = name;
   this.maxCustomer = maxCustomer;
   this.minCustomer = minCustomer;
   this.avgCookiePerHour = avgCookiePerHour;
   this.cookieTotal = [];
-  console.log('constructor works', CookieStore);
 }
 CookieStore.prototype.getRandomCustomer = function(){
   return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1)) + this.minCustomer;
@@ -84,7 +43,7 @@ CookieStore.prototype.listItem = function(){
   table.appendChild(tableBody);
 };
 
-function createTable(){
+CookieStore.prototype.createTable = function(){
   var table = document.createElement('table');
   app.appendChild(table);
   var tablehead = document.createElement('thead');
@@ -98,11 +57,8 @@ function createTable(){
     tableRow.appendChild(th);
     th.innerText = storeHourArray[i];
   }
-}
-createTable();
-
-
-
+};
+CookieStore.prototype.createTable();
 
 var pikeStore = new CookieStore('1st and Pike', 65, 23, 6.3);
 pikeStore.listItem();
@@ -114,3 +70,22 @@ var capitolHillStore = new CookieStore('Capitol Hill',38,20,2.3);
 capitolHillStore.listItem();
 var alkiStore = new CookieStore('Alki',16,2,4.6);
 alkiStore.listItem();
+
+var form = document.getElementById('New-Store-Form');
+function createNewStoreOnSubmit(event){
+  event.preventDefault();
+  var theForm = event.target;
+  var name = theForm.elements['name'].value;
+  var maxCustomer = Math.floor(theForm.elements['maxCustomer'].value);
+  var minCustomer = Math.floor(theForm.elements['minCustomer'].value);
+  var avgCookiePerHour = theForm.elements['avgCookiePerHour'].value;
+  if(maxCustomer < minCustomer){
+    alert('Maximum customer is less than minimum customer');
+    console.log('if statement works');
+  } else {
+    var userStore = new CookieStore(name, maxCustomer, minCustomer, avgCookiePerHour);
+    userStore.listItem();
+  }
+  theForm.reset();
+};
+form.addEventListener('submit', createNewStoreOnSubmit);
