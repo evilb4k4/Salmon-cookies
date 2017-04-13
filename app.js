0879908d33d5e7353dc25c1099298c0a180d2736
Adan.Rabadan <evilb4k4@gmail.com>
2017-04-13 10:01:24 -0700

    updating my css for the form

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var storeHourArray = ['','6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm','Total'];
+var storeHourArray = ['Store Hours','6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm','Total'];
 var app = document.getElementById('app');
 
 function CookieStore(name, maxCustomer, minCustomer, avgCookiePerHour) {
